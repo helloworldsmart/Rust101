5b796ca45e373a648bafe27bf035a12803fa11ad
MichaelChen <helloworldsmart3@gmail.com>
2022-11-28 20:54:05 +0800

    Feat: add quickreplace

---
+++ ProgrammingRust/Chapter2/quickreplace/src/main.rs
@@ -0,0 +1,38 @@
+#[deriver(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
+use text_colorizer::*;
+fn print_usage() {
+    eprintln!("{} - change occurrences of one string into another", "quickreplace".green());
+    eprintln!("Usage: quickreplace <targe> <replacement> <INPUT> <OUTPUT>");
+}
+
+use std::env;
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().ship(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!("{} wrong number of argument: expected 4, got {}.",
+            "Error:".red().bold(), args.len());
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
+}
+
+fn main() {
+    let args = parse_args();
+    println!("{:?}", args);
+}
