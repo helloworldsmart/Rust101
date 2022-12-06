31ea6eaa05ef21359276cc63a94f7504e251819f
MichaelChen <helloworldsmart3@gmail.com>
2022-12-06 23:33:21 +0800

    Feat: variables add shadowing

--- TheRustProgrammingLanguage/Chapter3/variables/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut x = 5;
-    println!("x 的數值為：{}", x);
-    x = 6;
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("x 在內部範圍的數值為：{}", x);
+    }
     println!("x 的數值為：{}", x); 
 }
