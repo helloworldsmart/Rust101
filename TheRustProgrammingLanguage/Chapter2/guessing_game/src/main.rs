1e99406a9d5361b05e387abc294b640287fc4e77
MichaelChen <helloworldsmart3@gmail.com>
2022-12-06 21:34:19 +0800

    Feat: guessing_game

--- TheRustProgrammingLanguage/Chapter2/guessing_game/src/main.rs
+++ TheRustProgrammingLanguage/Chapter2/guessing_game/src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("請猜測一個數字!");
 
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("祕密數字為：{}", secret_number);
+    
     println!("請輸入你的猜測數字。");
 
     let mut guess = String::new();
