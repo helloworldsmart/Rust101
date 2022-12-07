d470a2c251923272560bf63d1de2baa91231c1b5
MichaelChen <helloworldsmart3@gmail.com>
2022-12-07 23:08:20 +0800

    Feat: data-type add numerical operations

--- TheRustProgrammingLanguage/Chapter3/data-type/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/data-type/src/main.rs
@@ -7,4 +7,15 @@ fn main() {
     let x = 2.0; // f63
 
     let y: f32 = 3.0; // f32
+
+    let sum = 5 + 10;
+
+    let difference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3;
+
+    let remainder = 43 % 5;
 }
