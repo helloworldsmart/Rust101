445bec71e725c3090e8e0cd7d849bff1379b145c
MichaelChen <helloworldsmart3@gmail.com>
2022-12-07 22:47:42 +0800

    Feat: variables talk about shadowing

--- TheRustProgrammingLanguage/Chapter3/variables/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/variables/src/main.rs
@@ -7,5 +7,13 @@ fn main() {
         let x = x * 2;
         println!("x 在內部範圍的數值為：{}", x);
     }
-    println!("x 的數值為：{}", x); 
+    println!("x 的數值為：{}", x);
+
+    let spaces = "123";
+    let spaces = spaces.len();
+    println!("spaces：{}", spaces); 
+
+    // Error: expected `&str`, found `usize`
+    // let mut spaces = "123";
+    // spaces = spaces.len();
 }
