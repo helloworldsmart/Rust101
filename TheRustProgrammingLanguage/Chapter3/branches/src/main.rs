bcca4356ed75d0bb5342f5d67cc13ac2c97160b9
MichaelChen <helloworldsmart3@gmail.com>
2022-12-17 15:38:00 +0800

    Feat: branches add mismatched types

--- TheRustProgrammingLanguage/Chapter3/branches/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/branches/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
-    let number = 3;
+    let number = 7;
 
     if number < 5 {
         println!("條件為真");
     } else {
         println!("條件為否");
+    }
+
+    // let number = 3;
+
+    // mismatched types
+    // not Javascript
+    // if number {
+    //     println!("數字為三");
+    // }
+
+    let number = 3;
+
+    if number != 0 {
+        println!("數字不為零");
     }
 }
