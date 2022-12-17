82a23bf32385d5794992b145e9cbbe7fdc9a9b9c
MichaelChen <helloworldsmart3@gmail.com>
2022-12-17 14:47:16 +0800

    Feat: data-type another_function add arguments

--- TheRustProgrammingLanguage/Chapter3/functions/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5);
 }
 
-fn another_function() {
-    println!("另一支函式。")
+fn another_function(x: i32) {
+    println!("x 的數值為：{}", x)
 }
