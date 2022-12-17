79babef3883f71f468b3af685969bafe8785e899
MichaelChen <helloworldsmart3@gmail.com>
2022-12-17 15:01:05 +0800

    Feat: another_function add print_labeld_measurement

--- TheRustProgrammingLanguage/Chapter3/functions/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/functions/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5);
+
+    print_labeld_measurement(5, 'h')
 }
 
 fn another_function(x: i32) {
-    println!("x 的數值為：{}", x)
+    println!("x 的數值為：{}", x);
+}
+
+fn print_labeld_measurement(value: i32, unit_label: char) {
+    println!("測量值爲：{}{}", value, unit_label);
 }
