76ffd473f393d51880f678c0ab80aebc7f6cf645
MichaelChen <helloworldsmart3@gmail.com>
2022-12-17 15:42:28 +0800

    Feat: branches handling Multiple Conditions with else if

--- TheRustProgrammingLanguage/Chapter3/branches/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/branches/src/main.rs
@@ -20,4 +20,16 @@ fn main() {
     if number != 0 {
         println!("數字不為零");
     }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("數字可以被 4 整除");
+    } else if number % 3 == 0 {
+        println!("數字可以被 3 整除");
+    } else if number % 2 == 0 {
+        println!("數字可以被 2 整除");
+    } else {
+        println!("數字無法被 4、3、2 整除");
+    }
 }
