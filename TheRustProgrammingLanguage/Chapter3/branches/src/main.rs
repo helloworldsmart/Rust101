0f2c27c325c123c57e55686738d056a183829a1b
MichaelChen <helloworldsmart3@gmail.com>
2022-12-17 16:49:26 +0800

    Feat: branches add loop

--- TheRustProgrammingLanguage/Chapter3/branches/src/main.rs
+++ TheRustProgrammingLanguage/Chapter3/branches/src/main.rs
@@ -64,4 +64,16 @@ fn main() {
         count += 1;
     }
     println!("End count = {}", count);
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("結果為：{}", result);
 }
