0d4594681836d983b5be55639591998d2ef81bbd
MichaelChen <helloworldsmart3@gmail.com>
2022-12-25 22:25:57 +0800

    Feat: mod restaurant add super

--- TheRustProgrammingLanguage/Chapter7/restaurant/src/lib.rs
+++ TheRustProgrammingLanguage/Chapter7/restaurant/src/lib.rs
@@ -27,6 +27,17 @@ pub fn eat_at_restaurant() {
     front_of_house::hosting::add_to_waitlist();
 }
 
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
