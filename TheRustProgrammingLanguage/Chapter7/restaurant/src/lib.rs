c3b1799a7cb3dfbb471819980f31cd267ee60f58
MichaelChen <helloworldsmart3@gmail.com>
2022-12-25 23:12:10 +0800

    Feat: mod restaurant add eat_at_restaurant

--- TheRustProgrammingLanguage/Chapter7/restaurant/src/lib.rs
+++ TheRustProgrammingLanguage/Chapter7/restaurant/src/lib.rs
@@ -19,23 +19,49 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // 絕對路徑
-    crate::front_of_house::hosting::add_to_waitlist();
+// pub fn eat_at_restaurant() {
+//     // 絕對路徑
+//     crate::front_of_house::hosting::add_to_waitlist();
 
-    // 相對路徑
-    front_of_house::hosting::add_to_waitlist();
-}
+//     // 相對路徑
+//     front_of_house::hosting::add_to_waitlist();
+// }
 
 fn serve_order() {}
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("桃子"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
     }
 
     fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // 點夏季早餐並選擇黑麥麵包
+    let mut meal = back_of_house::Breakfast::summer("黑麥");
+    // 我們想改成全麥麵包
+    meal.toast = String::from("全麥");
+    println!("我想要{}麵包，謝謝", meal.toast);
+
+    // 接下來這行取消註解的話，我們就無法編譯通過
+    // 我們無法擅自更改餐點搭配的季節水果
+    // meal.seasonal_fruit = String::from("藍莓");
 }
 
 #[cfg(test)]
