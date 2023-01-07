a49006f9a24aede320f3bd3afcb0aa1e322dbb01
MichaelChen <helloworldsmart3@gmail.com>
2023-01-07 21:42:38 +0800

    Feat: bringing paths into scope with the use keyword

--- TheRustProgrammingLanguage/Chapter7/restaurant/src/lib.rs
+++ TheRustProgrammingLanguage/Chapter7/restaurant/src/lib.rs
@@ -1,9 +1,19 @@
-use std::collections::HashMap
+use std::collections::HashMap;
+use std::fmt::Result;
+use std::io::Result as IoResult;
+use std::{cmp::Ordering, io};
 
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 }
+
+fn function1() -> Result {
+    //
+}
+
+fn function2() -> IoResult<()> {}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
