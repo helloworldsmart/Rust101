00b7082ea4ef49ba6790fe656d366f951066adb7
MichaelChen <helloworldsmart3@gmail.com>
2023-01-07 22:04:41 +0800

    Feat: add vectors

--- TheRustProgrammingLanguage/Chapter8/vectors/src/main.rs
+++ TheRustProgrammingLanguage/Chapter8/vectors/src/main.rs
@@ -1,8 +1,29 @@
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    // let v: Vec<i32> = Vec::new();
     // let v = vec![1, 2, 3];
+    let mut v = Vec::new();
     v.push(5);
     v.push(6);
     v.push(7);
     v.push(8);
+}
+
+fn example2() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("第三個元素是 {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("第三個元素是 {third}"),
+        None => println!("第三個元素並不存在。"),
+    }
+}
+
+fn example3() {
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
 }
